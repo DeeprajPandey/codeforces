7c6457c01b38030142905d04d0a4fd244e73957c
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-02-01 08:53:46 +0530

    Output in long long to prevent printing in exponential form.

--- theatresq.cpp
+++ theatresq.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 #include<math.h>
+#include<iomanip>
+
 using namespace std;
 int main()
 {
 	int n, m, a;
 	cin >> n >> m >> a;
 
-	cout << ceil(n / (double)a) * ceil(m / (double)a) << endl;
+	cout << (long long)(ceil(n / (double)a) * ceil(m / (double)a)) << endl;
 
 	return 0;
 }
