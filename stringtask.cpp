1d47a342263fc228ee9e634ed848cd30a6610585
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-02-01 13:13:42 +0530

    Solved String Task

---
+++ stringtask.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string s, output;
+	cin >> s;
+
+	transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+	for(int i=0; i<s.length(); i++)
+	{
+		if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u' && s[i]!='y')
+			output = output + "." + s[i];
+	}
+	cout << output;
+
+	return 0;
+}
